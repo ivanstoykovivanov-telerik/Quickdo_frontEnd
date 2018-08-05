4808f0ae0bb5746f070f2c2b31ba4c0e295bb1e6
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-08-06 01:31:47 +0300

    User Profile added

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from '../user/user.component';
 import { AddUserComponent } from '../user/add-user/add-user.component';
-
-
+import { ProfileComponent } from '../profile/profile.component';
 
 const routes: Routes = [
-  {path  : "users" , component : UserComponent}, 
-  {path  : "add" , component : AddUserComponent}, 
+  {path  : "tasks" , component : UserComponent}, 
+  {path  : "register", component : AddUserComponent}, 
+  {path  : "profile" , component : ProfileComponent}
 ]
-
 
 @NgModule({
   imports: [
