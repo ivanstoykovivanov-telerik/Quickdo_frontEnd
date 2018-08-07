e3a4cff14d27d0758063a9a1e7013f93a97e483b
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-08-07 11:12:43 +0300

    Drag and Drop functionality works

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -12,18 +12,40 @@ export class TasksComponent implements OnInit {
     {name: "Walk the horse", type: "vegetable"},
     {name: "Water the grass", type: "fruit"}];
     
-  droppedItems = [] ; 
+    itemsInProgress= [
+      {name: "Walk the itemsInProgress", type: "fruit"}
+    ]; 
+    itemsDone = [
+      {name: "Walk the itemsInProgress", type: "fruit"}
+    ]; 
+    droppedItems = []; 
+  
 
-  onItemDrop(e: any) {
+  onItemDroppedToInProgress(e: any) {
     // Get the dropped data here
     
     // TODO:  HERE connect to a service and make the changes in the db. 
 
     //delete the dragged item 
     this.items =  this.items.filter( i => i.name !== e.dragData.name );
-
-    this.droppedItems.push(e.dragData);
+    this.itemsInProgress.push(e.dragData);
+    console.log("From In progress:");
+    console.log(this.itemsInProgress);
   }
+
+  onItemDroppedToDone(e: any) {
+    
+    //delete item from in progress
+    console.log("From Dropped To Done: ");
+    console.log(this.itemsInProgress);
+    console.log("dragged Data: ");
+    console.log(e.dragData);
+    
+    this.itemsInProgress =  this.itemsInProgress.filter( i => i.name !== e.dragData.name );
+    this.itemsDone.push(e.dragData);
+    console.log(this.itemsDone);
+  }
+
   
   constructor() { }
 
